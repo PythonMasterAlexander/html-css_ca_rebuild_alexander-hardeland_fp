9aaad27695258df95f7252f7638c862912afd0e4
alexanderhardeland <alexander@hardeland.com>
2022-02-27 18:34:12 +0100

    Ready for delivery

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const locatePage = document.location.pathname;
+const navigationLinks = document.querySelectorAll('nav a');
+
+navigationLinks.forEach(links => {
+  if(links.href.includes(`${locatePage}`)) {
+    links.classList.add('activeLink');
+  }
+});
