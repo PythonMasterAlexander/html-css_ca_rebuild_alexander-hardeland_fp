b4b337199bfb2ee488000d43e31e3821a5645461
alexanderhardeland <alexander@hardeland.com>
2022-03-05 19:52:33 +0100

    Started on javascript code for the project. Made a form success html page

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//By Alexander Hardeland
 const locatePage = document.location.pathname;
 const navigationLinks = document.querySelectorAll('nav a');
 
@@ -6,3 +7,26 @@ navigationLinks.forEach(links => {
     links.classList.add('activeLink');
   }
 });
+
+//Error message
+ //Create a message for the user if input fields dont receive the right information
+const formErrorMessage = function() {
+
+  let htmlMessage = `<img src="/images/form-error-symbol.svg" style="margin-right: 10px">
+  <p style="display:inline-block; color: red; margin-top:10px">${fullNameErrorMessage}</p>`;
+  return htmlMessage;
+};
+
+//Create a onkeyup function that validate user input on the input field
+const inputFieldError = function(event) {
+  let lengthOfInputField = event.target.value.length;
+
+  if(lengthOfInputField <= 1) {
+    inputContainer.innerHTML = formErrorMessage();
+    input.style.border = `solid ${invalidBorderColor} 2px`;    
+
+  } else {
+    inputContainer.remove();
+    input.style.border = `solid ${validBorderColor} 2px`;
+  }
+};
