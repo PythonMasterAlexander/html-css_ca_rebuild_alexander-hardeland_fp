020fc49254bad9e750b6335eb6da7fdffa4af0bb
Alexander Hardeland <alexanderhardeland.gmail.com>
2022-03-15 20:17:29 +0100

    Added for loop to loop over input variable which was a nodeList. Then used a addeventListener to listen for the keyup event

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,6 +1,7 @@
 import { formContainer } from "./component/components.js";
 import { validateForm } from "./functions.js";
 import { fullNameErrorMessage } from "./component/components.js";
+import { input } from "./component/components.js";
 
 const inputErrorContainer = document.querySelector('.inputErrorContainer');
 
@@ -12,7 +13,13 @@ const errorTextMessage = document.createElement('p');
 errorTextMessage.classList.add('inputFieldErrorMessage');
 errorTextMessage.textContent = fullNameErrorMessage; 
 
-inputErrorContainer.append(imgHtmlElement, errorTextMessage);
+//A lesson, when using addEventListener we cant use 'on' before the event
+for(let i = 0; i < input.length; i++) {
+
+  input[i].addEventListener('keyup', function(event) {
+    let keyValueLength = event.target.value.length;
+  });
+}
 
 
 formContainer.addEventListener('submit', validateForm);
