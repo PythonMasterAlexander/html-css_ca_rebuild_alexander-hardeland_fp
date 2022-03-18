053e168bdabb443eea1b43b249fa87d5c37bbc07
alexanderhardeland <alexander@hardeland.com>
2022-03-18 13:59:58 +0100

    Working on forEach trying to get it to work

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,8 +3,21 @@ from "./component/components.js";
 
 import { validateForm } from "./functions.js";
 
-const inputErrorContainer = document.querySelector('.inputErrorContainer');
+const inputContainers = document.querySelectorAll('.inputContainer');
 
+Array.from(inputContainers).forEach(item => {
+
+  const errorContainers = document.createElement('div');
+  item.append(errorContainers);
+});
+
+Array.from(formInputFields).forEach(item => {
+  console.log(item);
+  item.addEventListener('keyup', function(event) {
+    console.log(event.target.value.length);
+  });
+});
+/*
 const imgHtmlElement = document.createElement('img');
 const errorTextMessage = document.createElement('p');
 
@@ -13,21 +26,25 @@ errorTextMessage.classList.add('inputFieldErrorMessage');
 
 imgHtmlElement.src = "/images/form-error-symbol.svg";
 errorTextMessage.textContent = fullNameErrorMessage; 
+*/
 
-Array.from(formInputFields).forEach(function(formField) {
-  console.log(formField);
 
-  formField.addEventListener('keyup', function(event) {
+/*
+Array.from(formInputFields).forEach(function(input) {
+  console.log(input);
+  input.addEventListener('keyup', function(event) {
     let keyValueLength = event.target.value.length;
-
     if(keyValueLength < 3) {
-      inputErrorContainer.append(imgHtmlElement, errorTextMessage);
-      formField.style.border = invalidBorderColor;
+      errorContainers.append(imgHtmlElement, errorTextMessage);
+      input.style.border = invalidBorderColor;
 
     } else {
-      inputErrorContainer.remove();
-      formField.style.border = validBorderColor;
+      errorContainers.remove();
+      input.style.border = validBorderColor;
     } 
-  })
+  });
 });
+*/
+
+
 formContainer.addEventListener('submit', validateForm);
